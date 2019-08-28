eb42dbf3a0fe2289bc540d42047e8b7bebff76c2
minoru.nagasawa <minoru.nagasawa@gmail.com>
2019-08-28 23:29:54 +0900

    Change the parameter to pass Attribute instead of string

--- SampleApplicationSettingsDialog/Settings.cs
+++ SampleApplicationSettingsDialog/Settings.cs
@@ -10,35 +10,35 @@ namespace SampleApplicationSettingsDialog.Properties
         public Settings()
         {
             // PropertyGridのCategoryを設定する
-            var categoryTable = new Dictionary<string, string>()
+            var categoryTable = new Dictionary<string, Attribute>()
             {
-                { nameof(Settings.BoolSetting),             "組み込みのデータ型" },
-                { nameof(Settings.StringSetting),           "組み込みのデータ型" },
-                { nameof(Settings.StringCollectionSetting), "複合データ型" },
-                { nameof(Settings.DateTimeSetting),         "複合データ型" },
-                { nameof(Settings.IntSetting),              "組み込みのデータ型" },
+                { nameof(Settings.BoolSetting),             new CategoryAttribute("組み込みのデータ型") },
+                { nameof(Settings.StringSetting),           new CategoryAttribute("組み込みのデータ型") },
+                { nameof(Settings.StringCollectionSetting), new CategoryAttribute("複合データ型") },
+                { nameof(Settings.DateTimeSetting),         new CategoryAttribute("複合データ型") },
+                { nameof(Settings.IntSetting),              new CategoryAttribute("組み込みのデータ型") },
             };
-            addCategory(categoryTable);
+            addAttribute(categoryTable);
 
             // PropertyGridのHelpテキストを設定する
-            var descriptionTable = new Dictionary<string, string>()
+            var descriptionTable = new Dictionary<string, Attribute>()
             {
-                { nameof(Settings.BoolSetting),             "bool型の設定" },
-                { nameof(Settings.StringSetting),           "string型の設定" },
-                { nameof(Settings.StringCollectionSetting), "複数のstring型の設定" },
-                { nameof(Settings.DateTimeSetting),         "DateTime型の設定" },
-                { nameof(Settings.IntSetting),              "int型の設定" },
+                { nameof(Settings.BoolSetting),             new DescriptionAttribute("bool型の設定") },
+                { nameof(Settings.StringSetting),           new DescriptionAttribute("string型の設定") },
+                { nameof(Settings.StringCollectionSetting), new DescriptionAttribute("複数のstring型の設定") },
+                { nameof(Settings.DateTimeSetting),         new DescriptionAttribute("DateTime型の設定") },
+                { nameof(Settings.IntSetting),              new DescriptionAttribute("int型の設定") },
             };
-            addDescription(descriptionTable);
+            addAttribute(descriptionTable);
         }
 
         /// <summary>
-        /// プロパティにCategory属性を追加する
+        /// プロパティに属性を追加する
         /// </summary>
-        /// <param name="categoryTable">Key:プロパティ名、Value:テキスト</param>
-        private void addCategory(Dictionary<string, string> categoryTable)
+        /// <param name="attributeTable">Key:プロパティ名、Value:追加する属性</param>
+        private void addAttribute(Dictionary<string, Attribute> attributeTable)
         {
-            if (categoryTable == null)
+            if (attributeTable == null)
             {
                 return;
             }
@@ -46,8 +46,8 @@ namespace SampleApplicationSettingsDialog.Properties
             var properties = TypeDescriptor.GetProperties(this);
             foreach (PropertyDescriptor p in properties)
             {
-                string text;
-                if (categoryTable.TryGetValue(p.Name, out text))
+                Attribute attribute;
+                if (attributeTable.TryGetValue(p.Name, out attribute))
                 {
                     // 属性にDescriptionAttributeを追加する。
                     // 本当はMemberDescriptor.Attributes.Addのようにしたいのだが、Attributes属性はgetだけ定義されている。
@@ -59,40 +59,7 @@ namespace SampleApplicationSettingsDialog.Properties
                     {
                         listAttr.AddRange(attrs);
                     }
-                    listAttr.Add(new CategoryAttribute(text));
-                    fi.SetValue(p.Attributes, listAttr.ToArray());
-                }
-            }
-        }
-
-        /// <summary>
-        /// プロパティにDescription属性を追加する
-        /// </summary>
-        /// <param name="descriptionTable">Key:プロパティ名、Value:テキスト</param>
-        private void addDescription(Dictionary<string, string> descriptionTable)
-        {
-            if (descriptionTable == null)
-            {
-                return;
-            }
-
-            var properties = TypeDescriptor.GetProperties(this);
-            foreach (PropertyDescriptor p in properties)
-            {
-                string text;
-                if (descriptionTable.TryGetValue(p.Name, out text))
-                {
-                    // 属性にDescriptionAttributeを追加する。
-                    // 本当はMemberDescriptor.Attributes.Addのようにしたいのだが、Attributes属性はgetだけ定義されている。
-                    // そのためリフレクションを使って属性を追加する
-                    var fi = p.Attributes.GetType().GetField("_attributes", BindingFlags.NonPublic | BindingFlags.Instance);
-                    var attrs = fi.GetValue(p.Attributes) as Attribute[];
-                    var listAttr = new List<Attribute>();
-                    if (attrs != null)
-                    {
-                        listAttr.AddRange(attrs);
-                    }
-                    listAttr.Add(new DescriptionAttribute(text));
+                    listAttr.Add(attribute);
                     fi.SetValue(p.Attributes, listAttr.ToArray());
                 }
             }
